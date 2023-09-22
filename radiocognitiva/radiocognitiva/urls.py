fa8427010d94c0e27da7e81d16d047cfcbc64096
nicoler229 <segundoue27@gmail.com>
2023-09-21 20:06:49 -0400

    2023-9-21

--- radiocognitiva/radiocognitiva/urls.py
+++ radiocognitiva/radiocognitiva/urls.py
@@ -21,6 +21,7 @@ from myapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', views.home_view, name='home'),
     path('establecer-rango/', views.establecer_rango, name='establecer-rango'),
     path('ver-rangos/', views.ver_rangos, name='ver-rangos')
 ]
